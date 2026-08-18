0203083619aa5fe1ad18c071700915851f33e374
RaedHawsah <raed.hawsah@example.com>
2026-08-18 13:53:06 +0300

    Slow down interactive background animation

--- script.js
+++ script.js
@@ -346,10 +346,10 @@ function initBackground() {
       // Update Nebula
       currentMouse.lerp(targetMouse, 0.03);
       nebulaUniforms.iMouse.value.copy(currentMouse);
-      nebulaUniforms.iTime.value = time;
+      nebulaUniforms.iTime.value = time * 0.3; // Slowed down nebula rotation
       
       // Update Particles
-      particleMaterial.uniforms.uTime.value += 0.05;
+      particleMaterial.uniforms.uTime.value += 0.015; // Slowed down particle wave speed
       
       camera.lookAt(scene.position);
       renderer.render(scene, camera);
